0ef4cb6d4b2d7e07498bcde7f6117052c2d75a6d
Jerome Briot <jbtechlab@gmail.com>
2020-03-28 13:59:11 +0100

    Refactor code Enable Run At Startup

--- MemoryUsed.py
+++ MemoryUsed.py
@@ -15,90 +15,77 @@ ui = adsk.core.UserInterface.cast(None)
 palette = adsk.core.Palette.cast(None)
 ctrl = adsk.core.CommandControl.cast(None)
 
-process = []
+process = None
 
-mem_min = 10000000000000000000
-mem_max = 0
+memoryMin = 10000000000000000000
+memoryMax = 0
 
 handlers = []
 stopFlag = None
 myCustomEvent = 'MyCustomEventId'
 customEvent = None
 
-this_addin_name = 'MemoryUsed'
-this_addin_version = '0.4.0'
-this_addin_author = 'Jerome Briot'
-this_addin_contact = 'jbtechlab@gmail.com'
+thisAddinName = 'MemoryUsed'
+thisAddinVersion = '0.5.0'
+thisAddinAuthor = 'Jerome Briot'
+thisAddinContact = 'jbtechlab@gmail.com'
 
-# The event handler that responds to the custom event being fired.
+
 class ThreadEventHandler(adsk.core.CustomEventHandler):
     def __init__(self):
         super().__init__()
     def notify(self, args):
-        global palette
-        global process
-        global mem_max, mem_min
-        # global customEvent, handlers, stopFlag, app, myCustomEvent
+
+        global memoryMax, memoryMin
 
         try:
 
             meminfo = process.memory_full_info()
 
-            if (meminfo.uss < mem_min):
-                mem_min = meminfo.uss
+            if (meminfo.uss < memoryMin):
+                memoryMin = meminfo.uss
 
-            if (meminfo.uss > mem_max):
-                mem_max = meminfo.uss
+            if (meminfo.uss > memoryMax):
+                memoryMax = meminfo.uss
+
+            palette = ui.palettes.itemById(thisAddinName + 'Palette')
 
             if palette:
-                #TODO check returned value
-                palette.sendInfoToHTML('send','{{"uss":{}, "min":{}, "max":{}}}'.format(meminfo.uss, mem_min, mem_max))
+                palette.sendInfoToHTML('send','{{"uss":{}, "min":{}, "max":{}}}'.format(meminfo.uss, memoryMin, memoryMax))
 
         except:
             if ui:
-                ui.messageBox('Failed:\n{}'.format(traceback.format_exc()), this_addin_name, 0, 0)
-
-            # if handlers.count:
-                # customEvent.remove(handlers[0])
-            # stopFlag.set()
-            # app.unregisterCustomEvent(myCustomEvent)
+                ui.messageBox('Failed:\n{}'.format(traceback.format_exc()), thisAddinName, 0, 0)
 
 
-# The class for the new thread.
 class MyThread(threading.Thread):
     def __init__(self, event):
         threading.Thread.__init__(self)
         self.stopped = event
 
     def run(self):
-        global app, palette
 
-        # Every second fire a custom event, passing a random number.
         while not self.stopped.wait(1):
             app.fireCustomEvent(myCustomEvent)
 
-# Event handler for the commandExecuted event.
+
 class ShowPaletteCommandExecuteHandler(adsk.core.CommandEventHandler):
     def __init__(self):
         super().__init__()
     def notify(self, args):
-        global palette, ui
+
         try:
 
-            cmdDef = ui.commandDefinitions.itemById('showMemoryMonitor')
-            if palette.isVisible:
-                palette.isVisible = False
-                ctrl.commandDefinition.name = 'Show memory monitor'
-                cmdDef.name = 'Show memory monitor'
+            palette = ui.palettes.itemById(thisAddinName + 'Palette')
+            if not palette:
+                palette = ui.palettes.add(thisAddinName + 'Palette', 'Memory used', 'MUPalette.html', True, True, True, 300, 215)
             else:
                 palette.isVisible = True
-                ctrl.commandDefinition.name = 'Hide memory monitor'
-                cmdDef.name = 'Hide memory monitor'
 
         except:
-            ui.messageBox('Command executed failed: {}'.format(traceback.format_exc()), this_addin_name, 0, 0)
+            ui.messageBox('Command executed failed: {}'.format(traceback.format_exc()), thisAddinName, 0, 0)
 
-# Event handler for the commandCreated event.
+
 class ShowPaletteCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
     def __init__(self):
         super().__init__()
@@ -110,74 +97,69 @@ class ShowPaletteCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
             command.execute.add(onExecute)
             handlers.append(onExecute)
         except:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()), this_addin_name, 0, 0)
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()), thisAddinName, 0, 0)
 
 def run(context):
 
-    global ui, app, palette, ctrl
+    global ui, app, ctrl
     global process
     global customEvent, stopFlag
 
+    ui = None
+
     try:
+
         app = adsk.core.Application.get()
         ui  = app.userInterface
 
-        # Register the custom event and connect the handler.
         customEvent = app.registerCustomEvent(myCustomEvent)
         onThreadEvent = ThreadEventHandler()
         customEvent.add(onThreadEvent)
         handlers.append(onThreadEvent)
 
-        # Create a new thread for the other processing.
         stopFlag = threading.Event()
         myThread = MyThread(stopFlag)
         myThread.start()
 
         qatRToolbar = ui.toolbars.itemById('QATRight')
 
-        showPaletteCmdDef = ui.commandDefinitions.addButtonDefinition('showMemoryMonitor', 'Show memory monitor', 'Display memory used by Fusion 360', './resources')
+        showPaletteCmdDef = ui.commandDefinitions.addButtonDefinition('showMemoryUsed', 'Memory used', 'Display memory used by Fusion 360', './resources')
 
-        # Connect to Command Created event.
         onCommandCreated = ShowPaletteCommandCreatedHandler()
         showPaletteCmdDef.commandCreated.add(onCommandCreated)
         handlers.append(onCommandCreated)
 
         ctrl = qatRToolbar.controls.addCommand(showPaletteCmdDef, 'HealthStatusCommand', False)
 
-        palette = ui.palettes.add('MemoryUsedPalette', 'Memory used', 'MUPalette.html', False, True, True, 300, 215)
-        palette.setPosition(400,400)
-        palette.isVisible = False
-
         process = psutil.Process()
 
     except:
         if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()), this_addin_name, 0, 0)
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()), thisAddinName, 0, 0)
 
 
 def stop(context):
 
-    global palette
-
     try:
+
+        palette = ui.palettes.itemById(thisAddinName + 'Palette')
+        if palette:
+            palette.deleteMe()
+
         if handlers.count:
             customEvent.remove(handlers[0])
         stopFlag.set()
         app.unregisterCustomEvent(myCustomEvent)
 
-        cmdDef = ui.commandDefinitions.itemById('showMemoryMonitor')
+        cmdDef = ui.commandDefinitions.itemById('showMemoryUsed')
         if cmdDef:
             cmdDef.deleteMe()
 
         qatRToolbar = ui.toolbars.itemById('QATRight')
-        cmd = qatRToolbar.controls.itemById('showMemoryMonitor')
+        cmd = qatRToolbar.controls.itemById('showMemoryUsed')
         if cmd:
             cmd.deleteMe()
 
-        palette = ui.palettes.itemById('MemoryUsedPalette')
-        if palette:
-            palette.deleteMe()
-
     except:
         if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()), this_addin_name, 0, 0)
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()), thisAddinName, 0, 0)
